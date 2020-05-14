0c20fd553f5b5e069eb88ff5a018685883213afc
MatthewJOwens <mjowens@mail.com>
2020-05-13 20:56:55 -0600

    errors from yesterday fixed, basic BorrowBook

---
+++ Models/Library.cs
@@ -0,0 +1,51 @@
+using console_library.Models;
+using System;
+using System.Collections.Generic;
+
+namespace console_library
+{
+  public class Library
+  {
+
+    private List<Book> Books { get; set; } = new List<Book>();
+
+    public void PrintBooks()
+    {
+      Console.WriteLine("Available books:");
+      for (int i = 0; i < Books.Count; i++)
+      {
+        Console.WriteLine($"{i + 1}) {Books[i].Title} - {Books[i].Author}");
+      }
+      Console.WriteLine("Select a book number to check out. Or (Q)uit or (R)eturn a book.");
+      string selection = Console.ReadLine();
+      int index;
+      if (int.TryParse(selection, out index) && Books[index] != null && Books[index].Available == true)
+      {
+        BorrowBook(index);
+      }
+      else
+      {
+        switch (selection.ToLower())
+        {
+          case "q":
+          case "quit":
+            break;
+          case "r":
+          case "return":
+            break;
+          default:
+            Console.WriteLine("Invalid selection.");
+            break;
+        }
+      }
+    }
+    public void AddBook(Book book)
+    {
+      Books.Add(book);
+    }
+    public void BorrowBook(int i)
+    {
+      Books[i - 1].Available = false;
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -7,8 +7,18 @@ namespace console_library
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      Console.WriteLine("Welcome to the library!");
       Book whereTheSidewalkEnds = new Book("Where the Sidewalk Ends", "Shel Silverstein");
+      Book nameOfTheWind = new Book("Name of the Wind", "Patrick Rothfuss");
+      Book houseOfChains = new Book("House of Chains", "Steven Erikson");
+      Book gardensOfTheMoon = new Book("Gardens of the Moon", "Steven Erikson");
+      Library myLibrary = new Library();
+      myLibrary.AddBook(whereTheSidewalkEnds);
+      myLibrary.AddBook(nameOfTheWind);
+      myLibrary.AddBook(houseOfChains);
+      myLibrary.AddBook(gardensOfTheMoon);
+      myLibrary.PrintBooks();
+
     }
   }
 }
